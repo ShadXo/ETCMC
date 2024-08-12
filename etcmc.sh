652a842f8dc1e06a4c2b15ce269404064e6c3ad1
ShadXo <gold_guver@hotmail.com>
2024-08-12 17:59:36 +0200

    Add new feature node updater

--- etcmc.sh
+++ etcmc.sh
@@ -89,7 +89,7 @@ echo -e "6) INSTALL NEW NODES" #  -> etcmc_SETUP.SH" # OK
 echo -e "7) CHECK NODES STATUS (NOTDONEYET)" #  -> etcmc_CHECK_STATUS.SH" # OK
 echo -e "8) RESYNC SPECIFIC NODE (useful if node is stopped) (NOTDONEYET)" # -> etcmc_RESYNC.sh # OK
 echo -e "9) REMOVE SPECIFIC NODE" # -> etcmc_REMOVE.sh # OK
-echo -e "10) UPDATE NODE WALLET (NOTDONEYET)" # -> UPDATE_WALLET.sh # OK
+echo -e "10) UPDATE NODE" # -> UPDATE_NODE.sh # OK
 echo -e "11) UPDATE WALLET ADDNODES (NOTDONEYET)" # -> UPDATE_ADDNODES.sh # OK
 echo -e "12) NODE INFO (DO NOT SHARE WITHOUT REMOVING PRIVATE INFO) (NOTDONEYET)" # -> etcmc_info.sh # OK
 echo -e "13) FORK FINDER (NOTDONEYET)" # -> find_fork.sh # OK
---
+++ update_node.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+
+# Execute getopt
+ARGS=$(getopt -o "p:n:" -l "project:,node:" -n "$0" -- "$@");
+
+eval set -- "$ARGS";
+
+while true; do
+    case "$1" in
+        -p |--project)
+            shift;
+                    if [ -n "$1" ];
+                    then
+                        NAME="$1";
+                        shift;
+                    fi
+            ;;
+        -n |--node)
+            shift;
+                    if [ -n "$1" ];
+                    then
+                        ALIAS="$1";
+                        shift;
+                    fi
+            ;;
+        --)
+            shift;
+            break;
+            ;;
+    esac
+done
+
+# Check required arguments
+if [ -z "$NAME" ]; then
+    echo "You need to specify a project, use -p or --project to do so."
+    echo "Example: $0 -p etcmc"
+    exit 1
+fi
+
+if [ -z "$ALIAS" ]; then
+  ALIAS="*"
+else
+  ALIAS=${ALIAS,,}
+fi
+
+# GET CONFIGURATION
+SETUP_CONF_FILE="./projects/${NAME}/${NAME}.env"
+#if [ `wget --spider -q https://raw.githubusercontent.com/ShadXo/ETCMC/master/projects/${NAME}/${NAME}.env` ]; then
+mkdir -p ./projects/${NAME}
+wget https://raw.githubusercontent.com/ShadXo/ETCMC/master/projects/${NAME}/${NAME}.env -O $SETUP_CONF_FILE > /dev/null 2>&1
+chmod 777 $SETUP_CONF_FILE &> /dev/null
+#dos2unix $SETUP_CONF_FILE > /dev/null 2>&1
+#fi
+
+if [ -f ${SETUP_CONF_FILE} ] && [ -s ${SETUP_CONF_FILE} ]; then
+  echo "Using setup env file: ${SETUP_CONF_FILE}"
+  source "${SETUP_CONF_FILE}"
+else
+  echo "No setup env file found, create one at the following location: ./projects/${NAME}/${NAME}.env"
+  exit 1
+fi
+
+## MAIN
+echo
+echo "${NAME^^} - Node updater"
+echo ""
+echo "Welcome to the ${NAME} Node update script."
+echo "Node v${NODEVERSION}"
+echo
+
+# Create Temp folder
+mkdir -p $CONF_DIR_TMP
+cd $CONF_DIR_TMP
+
+echo "Downloading the latest update files"
+UPDATEURL="https://github.com/Nowalski/ETCMC_Software/releases/download/Setup%2FWindows/update_files_linux.zip"
+if [[ $UPDATEURL == *.tar.gz ]]; then
+  wget ${UPDATEURL} -O update.tar.gz
+  WGET=$?
+elif [[ $UPDATEURL == *.zip ]]; then
+  wget ${UPDATEURL} -O update.zip
+  WGET=$?
+fi
+
+if [ $WGET -ne 0 ]; then
+  echo -e "${RED}Download failed, check the UPDATEURL.${NC}"
+  rm -rfd $CONF_DIR_TMP
+  exit 1
+fi
+
+if [[ $UPDATEURL == *.tar.gz ]]; then
+  #tar -xvzf ${WALLETDL} #-C ${WALLETDLFOLDER}
+  tar -xvzf update.tar.gz --exclude="config.toml"
+elif [[ $UPDATEURL == *.zip ]]; then
+  #unzip ${WALLETDL} #-d ${WALLETDLFOLDER}
+  unzip -o update.zip -x "config.toml"
+fi
+
+chmod 775 *
+#find . -type f -exec mv -t . {} + &> /dev/null # Some coins have files in subfolders
+#mv ./bin/${NAME}* /usr/bin
+#mv ./bin/${NAME}* /usr/local/bin # previous /usr/bin should be /usr/local/bin
+rm update.tar.gz update.zip &> /dev/null
+
+for FILE in $(ls -d ~/.${NAME}_$ALIAS | sort -V); do
+  NODEALIAS=$(echo $FILE | awk -F'[_]' '{print $2}')
+  VERSION=$(jq '.Version' .${NAME}_${NODEALIAS}/version.json)
+  NODECONFDIR=~/.${NAME}_${NODEALIAS}
+
+  # Compare the versions using dpkg
+  if dpkg --compare-versions "$VERSION" lt "$NODEVERSION"; then
+    echo "$NODEALIAS is running a lower node version $VERSION, updating this node to the latest version $NODEVERSION."
+  elif [ "$VERSION" = "$NODEVERSION" ]; then
+    echo "$NODEALIAS is already running the latest version $VERSION. Skipping this node."
+    break
+  else
+    echo "$NODEALIAS is running $VERSION, this version is unknown or not supported yet."
+    break
+  fi
+
+  GETHPID=`ps -ef | grep -i ${NAME} | grep -i -w ${NAME}_${NODEALIAS} | grep -v grep | awk '{print $2}'`
+  if [ "$GETHPID" ]; then
+    echo "Stopping Geth of Node $NODEALIAS. Please wait ..."
+    kill -SIGINT $GETHPID
+
+    # Wait for the process to exit
+    while ps -p "$GETHPID" > /dev/null; do
+      #echo "Please wait ..."
+      sleep 2
+    done
+  fi
+
+  NODEPID=`ps -ef | grep -i ${NAME} | grep -i -w ETCMC_GETH | grep -v grep | awk '{print $2}'`
+  if [ "$NODEPID" ]; then
+    echo "Stopping $NODEALIAS. Please wait ..."
+    systemctl stop ${NAME}_$NODEALIAS.service
+
+    # Wait for the process to exit
+    while ps -p "$NODEPID" > /dev/null; do
+      #echo "Please wait ..."
+      sleep 2
+    done
+  fi
+
+  echo "Copying update files to $NODECONFDIR."
+  cp * $NODECONFDIR
+
+  GETHPID=`ps -ef | grep -i ${NAME} | grep -i -w ${NAME}_${NODEALIAS} | grep -v grep | awk '{print $2}'`
+  NODEPID=`ps -ef | grep -i ${NAME} | grep -i -w ETCMC_GETH | grep -v grep | awk '{print $2}'`
+  if [ -z "$NODEPID" ]; then
+    echo "Starting $NODEALIAS."
+    systemctl start ${NAME}_$NODEALIAS.service
+  fi
+  sleep 2 # wait 2 seconds
+done
+
+# Remove Temp folder
+rm -rfd $CONF_DIR_TMP
+
+echo "Your nodes are now updated!"
